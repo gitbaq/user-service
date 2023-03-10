9edb9b91709aff20e108e95b00edebb60f5e4327
Syed Baqir Ali <gitbaq@gmail.com>
2023-03-10 12:18:55 +1100

    added web.xml

--- src/main/java/com/syedbaqirali/user/UserServiceApplication.java
+++ src/main/java/com/syedbaqirali/user/UserServiceApplication.java
@@ -2,11 +2,18 @@ package com.syedbaqirali.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 
-public class UserServiceApplication  {
+public class UserServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(UserServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
